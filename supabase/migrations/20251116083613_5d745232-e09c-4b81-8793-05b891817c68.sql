2b34519d8d723fc3308985376caf0663f3345c5c
AdilMunawar <adilfromavengers@gmail.com>
2025-11-28 03:16:50 +0500

    Merge conflicts resolved

--- supabase/migrations/20251116083613_5d745232-e09c-4b81-8793-05b891817c68.sql
+++
@@ -1,2 +0,0 @@
--- Make candidate_email nullable since email extraction may fail
-ALTER TABLE candidate_matches ALTER COLUMN candidate_email DROP NOT NULL;
--- supabase/migrations/20251127083627_bab2ded4-1649-4fd8-be66-eece13bd1605.sql
+++
@@ -1,3 +0,0 @@
--- Change years_of_experience from integer to numeric to support decimal values
-ALTER TABLE profiles 
-ALTER COLUMN years_of_experience TYPE numeric USING years_of_experience::numeric;
